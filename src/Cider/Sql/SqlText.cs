020bd28858cc0009d0022fa0560ffe2bbc302f04
Martin Zanoni <mza@vertica.dk>
2022-01-17 10:25:20 +0100

    SqlText

---
+++ src/Cider/Sql/SqlText.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cider.Sql;
+
+public class SqlText
+{
+    private readonly List<Condition> _conditions = new();
+
+    public SqlText IfWhere(bool predicate, string term)
+    {
+        return predicate
+            ? Where(term)
+            : this;
+    }
+
+    public SqlText IfNotWhere(bool predicate, string term) => IfWhere(!predicate, term);
+
+    public SqlText Where(string term)
+    {
+        if (!string.IsNullOrWhiteSpace(term))
+            _conditions.Add(new Condition(term));
+
+        return this;
+    }
+
+    public SqlText Where(Func<SqlText, SqlText> subWhere)
+    {
+        string sql = subWhere(Empty).BuildConditions();
+
+        _conditions.Add(new Condition(sql));
+
+        return this;
+    }
+
+    public SqlText OrWhere(string term)
+    {
+        if (!string.IsNullOrWhiteSpace(term))
+            _conditions.Add(new Condition(term, isOr: true));
+
+        return this;
+    }
+
+    public string BuildConditions()
+    {
+        if (!_conditions.Any())
+        {
+            return string.Empty;
+        }
+
+        var builder = new StringBuilder();
+
+        builder.Append($"({_conditions[0].Criteria})");
+
+        foreach (Condition condition in _conditions.Skip(1))
+        {
+            builder.Append(condition.IsOr ? " OR " : " AND ");
+
+            builder.Append($"({condition.Criteria})");
+        }
+
+        return builder.ToString();
+    }
+
+    public static SqlText Empty => new();
+
+    public override string ToString()
+    {
+        var sqlWhere = BuildConditions();
+
+        if (sqlWhere != string.Empty)
+            sqlWhere = $"WHERE {sqlWhere}";
+
+        return sqlWhere;
+    }
+
+    public static implicit operator string(SqlText where)
+    {
+        return where.ToString();
+    }
+
+    private class Condition
+    {
+        public Condition(string criteria, bool isOr = false)
+        {
+            Criteria = criteria;
+            IsOr = isOr;
+        }
+
+        public string Criteria { get; }
+        public bool IsOr { get; }
+    }
+}
---
+++ src/Tests/Sql/SqlTextTester.cs
@@ -0,0 +1,89 @@
+﻿using Cider.Sql;
+using Xunit;
+
+namespace Tests.Sql;
+
+public class SqlTextTester
+{
+    [Fact]
+    public void Where()
+    {
+        Assert.Equal(string.Empty, SqlText.Empty);
+    }
+
+    [Fact]
+    public void Where_1EQ1()
+    {
+        SqlText subject = SqlText.Empty.Where("1 = 1");
+
+        Assert.Equal("WHERE (1 = 1)", subject);
+    }
+
+    [Fact]
+    public void Where_1EQ1_OR2EQ2()
+    {
+        SqlText subject = SqlText.Empty
+            .Where("1 = 1")
+            .OrWhere("2 = 2");
+
+        Assert.Equal("WHERE (1 = 1) OR (2 = 2)", subject);
+    }
+
+    [Fact]
+    public void Where_1EQ1_AND_2EQ2()
+    {
+        SqlText subject = SqlText.Empty
+            .Where("1 = 1")
+            .Where("2 = 2");
+
+        Assert.Equal("WHERE (1 = 1) AND (2 = 2)", subject);
+    }
+
+    [Fact]
+    public void Where_SubWhere()
+    {
+        SqlText subject = SqlText.Empty
+            .Where("1 = 1")
+            .Where(w => w
+                .Where("X = X")
+                .Where("Y = Y"));
+
+        Assert.Equal("WHERE (1 = 1) AND ((X = X) AND (Y = Y))", subject);
+    }
+
+    [Fact]
+    public void IfWhere_True_1EQ1()
+    {
+        SqlText subject = SqlText.Empty
+            .IfWhere(true, "1 = 1");
+
+        Assert.Equal("WHERE (1 = 1)", subject);
+    }
+
+    [Fact]
+    public void IfWhere_False_IsEmpty()
+    {
+        SqlText subject = SqlText.Empty
+            .IfWhere(false, "1 = 1");
+
+        Assert.Equal(string.Empty, subject);
+    }
+
+    [Fact]
+    public void IfNotWhere_True_Empty()
+    {
+        SqlText subject = SqlText.Empty
+            .IfNotWhere(true, "1 = 1");
+
+        Assert.Equal(string.Empty, subject);
+    }
+
+    [Fact]
+    public void IfNotWhere_False_1EQ1()
+    {
+        SqlText subject = SqlText.Empty
+            .IfNotWhere(false, "1 = 1");
+
+        Assert.Equal("WHERE (1 = 1)", subject);
+    }
+}
